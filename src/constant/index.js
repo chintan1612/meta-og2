c8fdc284ad72c78afa77f056fde2216bc803a6ba
Chintan <chintanv@logisticinfotech.co.in>
2024-06-14 10:02:05 +0530

    .

--- src/constant/index.js
+++ src/constant/index.js
@@ -411,7 +411,7 @@ export const FAL_IMAGES_DIMENTIONS = [
 
 // export const FRONT_END_DOMAIN = "https://beta.tesseractx.com"
 // export const FRONT_END_DOMAIN = "https://dev.tesseractx.com";
-export const FRONT_END_DOMAIN = "https://meta-og2.vercel.app/";
+export const FRONT_END_DOMAIN = "https://meta-og2.vercel.app";
 
 // export const FRONT_END_DOMAIN = "http://127.0.0.1:3000"
 
