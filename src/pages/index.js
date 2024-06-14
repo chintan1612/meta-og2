778eab7be532a847598238cec702a5990b6a9241
Chintan <chintanv@logisticinfotech.co.in>
2024-06-14 14:30:04 +0530

    change meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 // import { FRONT_END_DOMAIN } from "@/constant";
 import PageTitle from "@/components/Common/PageTitle";
 import Head from "next/head";
-const projectTitle='TesseractX'
+const projectTitle = 'TesseractX'
 const FRONT_END_DOMAIN = 'https://meta-og2.vercel.app/'; // Replace with your domain
 const title = 'BITMATIC ART';
 const description = 'Your Page Description';
@@ -15,26 +15,41 @@ function HomePage() {
     return (
         <>
             <Head>
-            {/* <PageTitle
-                title={`${
-                    collectionDetails?.name || "NewCollection"
-                } - Collection`}
-                imageUrl={collectionDetails?.image}
-                id={collectionDetails?._id}
-            /> */}
-                
-                <meta name="description" content={description} />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content={FRONT_END_DOMAIN} />
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
-                <meta property="og:image" content={imageURL} />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content={FRONT_END_DOMAIN} />
-                <meta property="twitter:title" content={title} />
-                <meta property="twitter:description" content={description} />
-                <meta property="twitter:image" content={imageURL} />
-                <link rel="icon" href="/favicon.ico" />
+
+                <meta content="https://ipfs.io/ipfs/QmQSfMFJbciYKnhgLfMNr4Jw43ww5wV2j5AMDd2G8y8zVP" property="og:image" />
+                <meta content="No image show" property="og:image:alt" />
+                <meta content="HEART HEADS" name="application-name" />
+                <meta content="HEART HEADS" property="og:site_name" />
+                <meta content="website" property="og:type" />
+                <meta content="1200" property="og:image:width" />
+                <meta content="817" property="og:image:height" />
+                <meta content="HEART HEADS - Composable NFT's, Mint items and create your unique heart head on PulseChain!"
+                    property="og:description" />
+                <meta content="Heart Heads - NFT Collection, Mint items and create your unique heart head on PulseChain!"
+                    property="og:title" />
+
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:title"
+                    content="Heart Heads - NFT Collection, Mint items, and create your unique heart head on PulseChain!" />
+                <meta name="twitter:description"
+                    content="HEART HEADS - Composable NFT's, Mint items, and create your unique heart head on PulseChain!" />
+                <meta name="twitter:image" content="https://ipfs.io/ipfs/QmQSfMFJbciYKnhgLfMNr4Jw43ww5wV2j5AMDd2G8y8zVP" />
+
+
+
+                <meta content="HEART HEADS - Composable NFT's ,Mint items and create your unique heart head on PulseChain!"
+                    name="title" />
+                <meta content="HEART HEADS - Composable NFT's ,Mint items and create your unique heart head on PulseChain!"
+                    name="description" />
+                <meta content="HEART HEADS - Composable NFT's ,Mint items and create your unique heart head on PulseChain!"
+                    property="og:title" />
+                <meta content="HEART HEADS - Composable NFT's ,Mint items and create your unique heart head on PulseChain!"
+                    property="og:description" />
+                <meta content={FRONT_END_DOMAIN} property="og:url" />
+                <meta content="HEART HEADS - Composable NFT's ,Mint items and create your unique heart head on PulseChain!"
+                    name="keywords" />
+
+
             </Head>
             <div>
                 Your Content Here
