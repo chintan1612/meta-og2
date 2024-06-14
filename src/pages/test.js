b458a627f64d1b64f9aca9b43d31377ecb1e9b23
Chintan <chintanv@logisticinfotech.co.in>
2024-06-14 14:21:31 +0530

    c11

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,30 +1,27 @@
-// import { FRONT_END_DOMAIN } from "@/constant";
-import PageTitle from "@/components/Common/PageTitle";
 import Head from "next/head";
-const projectTitle='TesseractX'
+
+// Constants for the page
 const FRONT_END_DOMAIN = 'https://meta-og2.vercel.app'; // Replace with your domain
 const title = 'BITMATIC ART';
 const description = 'Your Page Description';
 const imgUrl = 'https://d17ha18jyelis7.cloudfront.net/collections/originals/16395541-68bd-4f66-a581-6528fb9ffbf4-1705050528582';
 
-const imageURL = `${FRONT_END_DOMAIN}/api/og?title=${title}&description=${description}&imageUrl=${imgUrl}`;
+// Constructing the full URL for the dynamically generated image
+const imageURL = `${FRONT_END_DOMAIN}/api/og?title=${encodeURIComponent(title)}&description=${encodeURIComponent(description)}&imageUrl=${encodeURIComponent(imgUrl)}`;
 
-
-console.log('imageURL', imageURL)
-function testPage() {
+function TestPage() {
     return (
         <>
             <Head>
-        
-                <title>Test</title>
+                <title>{title}</title>
                 <meta name="description" content={description} />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content={FRONT_END_DOMAIN + '/test'}  />
+                <meta property="og:url" content={`${FRONT_END_DOMAIN}/test`} />
                 <meta property="og:title" content={title} />
                 <meta property="og:description" content={description} />
                 <meta property="og:image" content={imageURL} />
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content={FRONT_END_DOMAIN + '/test'} />
+                <meta property="twitter:url" content={`${FRONT_END_DOMAIN}/test`} />
                 <meta property="twitter:title" content={title} />
                 <meta property="twitter:description" content={description} />
                 <meta property="twitter:image" content={imageURL} />
@@ -37,4 +34,4 @@ function testPage() {
     );
 }
 
-export default testPage;
+export default TestPage;
