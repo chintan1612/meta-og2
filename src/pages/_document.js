7f18d28c352f49e00ec8067c74e5f80a2703c442
Chintan <chintanv@logisticinfotech.co.in>
2024-06-14 13:47:30 +0530

    c7

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,19 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/FavLogo.ico" />
         <link rel="manifest" href="/manifest.json" />
-        <PageTitle
-                title={projectTitle}
-                imageUrl={imageURL}
-                description={description}
-            />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={FRONT_END_DOMAIN} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageURL} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={FRONT_END_DOMAIN} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={imageURL} />
+        <link rel="icon" href="/favicon.ico" />
         <script
           src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
           async=""
